7f1eea8c897758974875fb379ba9c1f68ef0e625
¨lucia_ab¨ <luabramzon@gmail.com>
2022-06-22 20:29:28 -0300

    desafio1

--- desafio_2.js
+++ desafio_2.js
@@ -1,5 +1,6 @@
 
 let productos=[]
+var fs=require("fs")
 
  class Contenedor{
     constructor(tittle,price,image){
@@ -10,55 +11,62 @@ let productos=[]
         Contenedor.id ++
     }
     static id = 0
-}
 
-    productos.push (new Contenedor ("jarron",105,"https://www.shopmania.es/q-jarrones"))
-    productos.push (new Contenedor ("plato",1305,"https://www.shopmania.es/q-jarrones"))
-        productos.push (new Contenedor ("mesa",1650,"https://www.shopmania.es/q-jarrones"))
-
-        var fs=require("fs")
- function save (){
-        fs.appendFileSync("productos.txt",JSON.stringify(productos),function(err){
-            if (err) throw err;
+    async save () {
+        try{
+            await fs.appendFileSync('productos.txt',JSON.stringify(productos))
             console.log("guardado")
-        })
-    }
- save()
- 
-   function getById(){
-    let elegido= productos.find(id=>id.id===2)
-    if(!elegido){
+        }
+        catch (err){
+            console.log("huo un error")
+        }
+        try{
+            let elegido=await productos.find(id=>id.id===2)
+            if(!elegido){
         console.log('no hay producots')
-    }else{
-        console.log(elegido)
-    }
-    }
-    getById()  
-
-    function getAll(){
-        fs.promises.readFile('./productos.txt','utf-8')
-        .then(contenido => {
+        }else{
+            console.log(elegido)
+        }
+        }
+        catch (err){
+            console.log("error")
+        }
+        try{
+            const contenido= await fs.promises.readFile('./productos.txt','utf-8')
             console.log(contenido)
-        })
-        .catch(err => {
-            console.log('Hubo un error de lectura')
-        })
+        }
+        catch (err){
+            console.log("no se puede leer archivo")
+        }
+        try{
+            productos.splice(0,1)
+            setTimeout(()=>{
+                fs.promises.writeFile('./productos.txt',JSON.stringify(productos))
+                console.log("guardados los cambios")
+            },5000)
+           
+        }
+        catch (err){
+            console.log("No existe el archivo a eliminar")
+        }
+        try{
+            setTimeout(()=>{
+                fs.unlinkSync("./productos.txt")
+                console.log("borrado todo")
+            },10000)
+        }
+        catch (err){
+            console.log(err)
+        }
     }
-getAll() 
+    
+   
+ }
 
-function deleteAll(){
-        fs.unlinkSync("./productos.txt")
-    }
-    deleteAll()
-
-    function deleteById(){
-        productos.splice(0,1)
-        fs.writeFile('./productos.txt',JSON.stringify(productos),function (err){
-            if (err) throw err;
-            else{
-                console.log("guardado")
-            }
-        })
-    }
-    deleteById()
+    productos.push (new Contenedor ("plato",1305,"https://www.shopmania.es/q-jarrones"))
+    productos.push (new Contenedor ("mesa",1650,"https://www.shopmania.es/q-jarrones"))
+    
+    const producto1= new Contenedor ("jarron",105,"https://www.shopmania.es/q-jarrones")
+    productos.push(producto1)
+    producto1.save()
 
