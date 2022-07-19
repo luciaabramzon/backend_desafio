89a24153d4d7d90011f9b86327cce3bd0335118e
¨lucia_ab¨ <luabramzon@gmail.com>
2022-07-18 21:00:06 -0300

    desafio4

--- desafio4.js
+++ desafio4.js
@@ -1,4 +1,4 @@
-
+// clase contenedor desafio 2
 let productos=[]
 var fs=require("fs")
 
@@ -10,7 +10,7 @@ var fs=require("fs")
         this.id= Contenedor.id
         Contenedor.id ++
     }
-    static id = 0
+    static id = 1
 
     async save () {
         try{
@@ -79,33 +79,100 @@ var fs=require("fs")
     const producto1= new Contenedor ("jarron",105,"https://www.shopmania.es/q-jarrones")
    productos.push(producto1) 
 /*    producto1.save()    */
-const express=require ('express')
-const { get } = require("https")
-const app=express()
-const port=process.env.PORT || 8000
 
-let inventario = async function(){
+//desafio 4
+
+const express=require ('express')
+const app=express()
+const PORT=process.env.PORT || 8080
+const {Router}= express
+const multer=require('multer')
+const { json } = require("body-parser")
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+const routerProductos= Router()
+app.use('/api/productos',routerProductos)
+
+app.get('/',async (req,res)=>{
+    res.end( await producto1.readFile())
+})
+
+app.get('/:id',async (req,res)=>{
+    const id =req.params.id 
     let inventario= await producto1.readFile()
     let contenidoArray=JSON.parse(inventario)
-    let number= contenidoArray[Math.floor(Math.random()*contenidoArray.length)]
-    console.log(number)
-    let getById = await producto1.getbyId(number.id)
-    console.log(getById)
-    return getById
-  }
-inventario()
- 
- app.get(`/`,async (req,res)=>{
-    res.end(`<h1> Desafio 3 </h1>`)
+    if(id>contenidoArray.length){
+        res.json({error: `El ID no existe`})
+    }
+    else{
+    let number=contenidoArray[id-1]
+    res.end(JSON.stringify(number))
+}
 })
 
-app.get(`/productos`,async (req,res)=>{
-    res.end(await producto1.readFile())
+app.post('/',(req,res)=>{
+    const producto=req.body
+    let productoNuevo= new Contenedor(producto)
+    productos.push(productoNuevo)
+    res.json(productos)
 })
 
-  app.get(`/productosRandom`,async (req,res)=>{
-    let aleatorio= await inventario()
-    res.end(JSON.stringify(aleatorio))
-})  
+app.put('/:id',async(req,res)=>{
+    const id=parseInt(req.params.id)
+    let inventario= await producto1.readFile()
+    let contenidoArray=JSON.parse(inventario)
+    if(id> contenidoArray.length){
+        res.json({error: `El ID no existe`})
+    } else { 
+        res.json({mensaje: `Cambiado producto ${id}`})
+    }
+})
+
+app.delete('/:id',async (req,res)=>{
+    const id=parseInt(req.params.id)
+    let inventario= await producto1.readFile()
+    let contenidoArray=JSON.parse(inventario)
+    if(id> contenidoArray.length){
+        res.json({error: `El ID no existe`})
+    } else { 
+    let idBorrar=contenidoArray.filter((item)=> item.id != id)
+    res.end(`Eliminado contenido id= ${id}`)
+}
+})
+
+const server=app.listen(PORT,()=>{
+    console.log(`Su app corre en el puerto ${PORT}`)
+})
+
+server.on("error",error=>console.log(`Error: ${error}`))
+
+//storage HTML
+
+/* app.get(`/`,(req,res)=>{
+    res.sendFile(__ + '/public/index.html')
+}) */
+
+let storage= multer.diskStorage({
+    destination:function(req,res,cb){
+        cb(null,'uploads')
+    },
+    filename: function(req,file,cb){
+        cb(null,file.fieldname+ '-'+ Date.now())
+    }
+})
+
+let upload=multer({sotrage:storage})
 
 
+app.post('/uploadfile', upload.single('myFile'),(req,res,next)=>{
+    const file=req.file
+    if(!file){
+        const error=new Error ('Please upload file')
+        error.httpStatusCode=400;
+        return next(error)
+    }
+    res.send(file)
+})
